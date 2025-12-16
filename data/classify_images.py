10ed7da6a2b979a2ff8d100f934c220d83c5144e
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2025-12-16 00:41:50 -0600

    implement todo 3

--- data/classify_images.py
+++ data/classify_images.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
+# PROGRAMMER: Ednalyn C. De Dios
+# DATE CREATED: December 16, 2025
 # REVISED DATE: 
 # PURPOSE: Create a function classify_images that uses the classifier function 
 #          to create the classifier labels and then compares the classifier 
@@ -65,4 +65,11 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    None 
+    for key in results_dic:
+        model_label = classifier(images_dir + key, model)
+        model_label.lower().strip()
+        truth = results_dic[key][0]
+        if truth in model_label:
+            results_dic[key].extend((model_label, 1))
+        else:
+            results_dic[key].extend((model_label, 0))
